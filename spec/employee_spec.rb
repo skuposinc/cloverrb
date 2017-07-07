6d642db9b35bf95679198b50dcb21d28b789aeb5
Arman Jon Villalobos <me@armanjon.com>
2017-07-07 11:09:54 +0800

    Add test for filtering employees

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,14 +1,20 @@
 require "spec_helper"
 
 RSpec.describe Cloverrb::Employee do
+  let(:token) { ENV['TEST_API_TOKEN'] }
+  let(:merchant_id) { ENV['TEST_MERCHANT_ID'] }
+
   describe "GET all employees" do
     token = ENV['TEST_API_TOKEN']
     merchant_id = ENV['TEST_MERCHANT_ID']
 
+    before do
+        @employee_client = described_class.new(token, merchant_id)
+    end
+
     it "should get all the employees" do
       VCR.use_cassette("get_all_employees", record: :once) do
-        employee_client = described_class.new(token, merchant_id)
-        employees = employee_client.all
+        employees = @employee_client.all
         employee = {
           "href"=>"https://www.clover.com/v3/merchants/CQWH7CZZVQ072/employees/1D5AGFDBF88G2",
           "id"=>"1D5AGFDBF88G2",
@@ -26,6 +32,14 @@ RSpec.describe Cloverrb::Employee do
 
         expect(employees["elements"]).to be_an_instance_of Array
         expect(employees["elements"]).to include(employee)
+      end
+    end
+
+    it "should properly filter the list of employees" do
+      VCR.use_cassette("filter_all_employers", record: :once) do
+        employees = @employee_client.all('EMPLOYEE')
+
+        expect(employees["elements"].count).to eq 4
       end
     end
   end  
