39d1e234fbc4c581e87a4d36f3fa5efdd50653c9
Arman Jon Villalobos <me@armanjon.com>
2017-07-07 13:05:33 +0800

    Add method to fetch an access token

--- lib/cloverrb/client.rb
+++ lib/cloverrb/client.rb
@@ -1,6 +1,7 @@
 module Cloverrb
   class Client
     BASE_URL = "https://api.clover.com/v3"
+    AUTH_URL = "https://clover.com/oauth/token"
 
     def get(token, path)
       HTTParty.get(BASE_URL + path, headers: build_headers(token)).parsed_response
@@ -22,10 +23,23 @@ module Cloverrb
       ).parsed_response
     end
 
+    def self.generate_access_token(client_id, code, app_secret)
+      query = build_query(client_id, code, app_secret)
+      HTTParty.get(AUTH_URL, query: query)
+    end
+
     private
 
     def build_headers(token)
       { "Authorization" => "Bearer #{token}"}
-    end  
+    end 
+
+    def self.build_query(client_id, code, app_secret)
+      {
+        "client_id" => client_id,
+        "client_secret" => app_secret,
+        "code" => code
+      }
+    end 
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Cloverrb::Client do
   it "has proper base url" do
     expect(described_class::BASE_URL).to eq "https://api.clover.com/v3"
   end
+
+  describe "generating an access token" do
+    it "should return a token" do
+      VCR.use_cassette("generate_access_token", record: :once) do
+        client_id = "738TXSSPWFQ8J"
+        code = "35be95bf-8c10-3783-9122-8a0b42a93493"
+        app_secret = "40f64a86-b4dc-a41f-7efa-7e1ab9ccf165"
+
+        response = described_class
+                    .generate_access_token(client_id, code, app_secret)
+                    .parsed_response
+
+        expect(response["access_token"]).to eq "1275d0e2-3430-5dc5-22fc-62e92bd53d73"
+      end
+    end
+  end
 end
