4b0f799fd99db46c65e7e8508435379ace625041
Scott Latham <silatham99@gmail.com>
2018-11-12 16:47:31 -0800

    Expand more payment fields

--- lib/cloverrb/payment.rb
+++ lib/cloverrb/payment.rb
@@ -1,12 +1,12 @@
 module Cloverrb
   class Payment < Client
     def self.all(merchant_id, token)
-      url = "/merchants/#{merchant_id}/payments?expand=refunds"
+      url = "/merchants/#{merchant_id}/payments?expand=refunds&expand=cardTransaction&expand=tender&expand=transactionInfo"
       get(token, url)
     end
 
     def self.find(merchant_id, token, payment_id)
-      url = "/merchants/#{merchant_id}/payments/#{payment_id}?expand=refunds"
+      url = "/merchants/#{merchant_id}/payments/#{payment_id}?expand=refunds&expand=cardTransaction&expand=tender&expand=transactionInfo"
       get(token, url)
     end
   end
