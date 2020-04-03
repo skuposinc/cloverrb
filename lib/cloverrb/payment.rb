6e1833f354497709bcf5a6947d91470f446a03f2
Krishnan Kannan <kxkannan@gmail.com>
2020-04-03 09:25:31 -0600

    Added options to the all method. (#1)
    * Added options to Payments.all method to handle dates and pagination.

--- lib/cloverrb/payment.rb
+++ lib/cloverrb/payment.rb
@@ -1,13 +1,39 @@
 module Cloverrb
   class Payment < Client
-    def self.all(merchant_id, token)
+    def self.all(merchant_id, token, options = {})
       url = "/merchants/#{merchant_id}/payments?expand=refunds&expand=cardTransaction&expand=tender&expand=transactionInfo"
+      url += add_date_filters(options)
+      url += add_limit_and_offset(options)
       get(token, url)
     end
 
     def self.find(merchant_id, token, payment_id)
       url = "/merchants/#{merchant_id}/payments/#{payment_id}?expand=refunds&expand=cardTransaction&expand=tender&expand=transactionInfo"
       get(token, url)
+    end
+
+    private
+
+    def self.add_date_filters(options)
+      filter = ""
+      if options["startDateTime"].present?
+        filter += "&filter=createdTime>=#{options['startDateTime']}"
+      end
+      if options["endDateTime"].present?
+        filter += "&filter=createdTime<=#{options['endDateTime']}"
+      end
+      filter
+    end
+
+    def self.add_limit_and_offset(options)
+      filter = ""
+      if options["limit"].present?
+        filter += "&limit=#{options['limit']}"
+      end
+      if options["offset"].present?
+        filter += "&offset=#{options['offset']}"
+      end
+      filter
     end
   end
 end
